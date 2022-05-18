4b9484946b783e1548743328449e602f2b213883
Sidney da Costa Santos Neto <sidney.neto@dominio01.rj>
2022-05-18 16:15:59 -0300

    until aula09

---
+++ aula07-constantes/index.js
@@ -0,0 +1,17 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar com o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase(iniciamos as palavras com letras minúsculas e as próximas com letra MAIÚSCULA)
+// Case-sensitive 
+// Não podemos modificar o valor de um constante
+// NÃO UTILIZE VAR, UTILIZE CONST.
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
+
---
+++ aula09-tiposDeDadosPrimitivos/index.js
@@ -0,0 +1,16 @@
+//tipos de dados primitivos: Strings , Number, Undefined, null, boolean.
+
+
+
+const nome = 'Sidney' ; //string
+const nome1 = "Sidney" ; //string
+const nome2 = `Sidney` ; //string
+const num1 = 10; // number
+const num2 = 10.52 // number
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno= null; // Nulo -> não aponta para locar nenhum na memória
+const aprovado =  true; // Boolean = true, false(valores lógicos)
+// comando typeof para ver qual o tipo da variável
+console.log(typeof nome, typeof num1);
+
+
