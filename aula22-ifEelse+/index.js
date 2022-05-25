8e16925a590f54b717a4c057528e72fb3e3ad17a
Sidney Neto <99205146+sidneynetto95@users.noreply.github.com>
2022-05-25 19:17:44 -0300

    aula 22 fim

--- aula22-ifEelse+/index.js
+++ aula22-ifEelse+/index.js
@@ -1,14 +1,30 @@
-/* if () {}  ............... SE
+/* if () {}  ...............SE
 testa SE a condição é verdadeira, se for verdadeira ela retorna o valor true e executa.
+Pode ser executado sozinho para coisas muito simples
+
+Pode ter varios else ifs na checagem
 O comando segue o seguinte padrão:  
 if (condição de teste) { função executada no bloco }
 
-  else if () {}  .............. E SE       
-continua o teste, vem sempre após um if, é uma forma de testar 
+  else if () {}  ............se não   
+Continua a função se o if não for executado
+Vem sempre após um if
+ 
+   else () {}; ................ou se
+  Necessario para fechar um if statement, executa se nenhuma outra condição for atendida.
+  Só pode ter um else na checagem
+
 */
 
-const hora = 10;
 
-if (hora < 12) {
+const hora = 19;
+
+if (hora >= 0 && hora <= 11) {
     console.log('Bom dia');
-}
+} else if (hora >= 12 && hora <= 17) {
+    console.log('Boa tarde')
+} else if ( hora >= 18 && hora <= 23) {
+    console.log('Boa noite')
+} else {
+  console.log('Olá!');
+};
