7d511edb719c71e6696ecf23b752f395d3db4ba4
Sidney Neto <sidneynetto.2014@gmail.com>
2023-12-21 15:37:27 -0300

    commit 2 - 21/12/2023

--- aula 38 - erro2/assets/js/main.js
+++ aula 38 - erro2/assets/js/main.js
@@ -1,3 +1,4 @@
+/*      EXEMPLO DE COMO PEGAR ERROS
 try {
     // é executado quando não há erros
 } catch (e) {
@@ -5,3 +6,29 @@ try {
 } finally {
     // é executado sempre
 }
+*/
+function retornaHora(data) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date.');
+    }
+    
+    if (!data) {
+        data= new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false
+    })
+}
+
+try{
+    const data = new Date('01-01-1970 12:58:12')
+    const hora = retornaHora();
+    console.log(hora);
+} catch (e){
+    //tratar erro
+    console.log(e);
+} finally {
+    console.log('Tenha um bom dia');
+}
+
